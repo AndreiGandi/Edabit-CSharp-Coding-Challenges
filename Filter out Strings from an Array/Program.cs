deea2f6c7301a4cf53d430a7e0b99552d4d2e7da
Andrei Gindilă <gandilaandrei@gmail.com>
2020-05-29 23:09:37 +0300

    Push2

---
+++ Filter out Strings from an Array/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter_out_Strings_from_an_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FilterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+            object[] haystack_1 = new object[] { "$%^", 567, "&&&" };
+            int[] nr = FilterArray(haystack_1);
+            for(int i=0; i<nr.Length; i++)
+            {
+                Console.Write($"{nr[i]} ");
+            }
+
+        }
+        public static int[] FilterArray(object[] arr)
+        {
+            System.Collections.ArrayList list = new System.Collections.ArrayList(arr.Length);
+            for(int i=0; i<arr.Length; i++)
+            {
+                list.Add(arr[i]);
+            }
+            IEnumerable<int> query1 = list.OfType<int>();
+            return query1.ToArray();
+        }
+        
+
+        //public static int[] FilterArray(object[] arr)
+        //{
+        //    List<int> list = new List<int>();
+        //    for(int i=0; i<arr.Length; i++)
+        //    {
+        //        int result;
+        //        if (int.TryParse(arr[i].ToString(), out result))
+        //        {
+        //            list.Add(result);
+        //        }
+        //    }
+        //    int[] nr = list.ToArray();
+        //    return nr;
+        //}
+    }
+}
---
+++ Is the Number a Prime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_the_Number_a_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IsPrime(5) ➞ true
+            //IsPrime(1) ➞ false
+            //IsPrime(23) ➞ true
+            //IsPrime(24) ➞ false
+            Console.WriteLine(IsPrime(-5));
+        }
+        public static bool IsPrime(int value)
+        {
+
+            if (value <= 1) return false;
+            else if (value != 2 && value % 2 == 0) return false;
+            else
+            {
+                for (int i = 2; i <= value / 2; i++)
+                {
+                    if (value % i == 0) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
