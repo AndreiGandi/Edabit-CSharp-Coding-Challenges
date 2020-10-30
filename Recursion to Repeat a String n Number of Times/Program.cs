eca3405b9d41deb2f5ff8ca28777cd8d3d71c27e
Andrei Gindilă <gandilaandrei@gmail.com>
2020-10-30 23:39:05 +0200

    Push 11

---
+++ Recursion to Repeat a String n Number of Times/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_to_Repeat_a_String_n_Number_of_Times
+{
+    class Program
+    {
+        //Create a recursive function that takes two parameters and repeats the string n number of times.
+        //The first parameter txt is the string to be repeated and the second parameter is the number of times the string is to be repeated.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Repetition("kiwi", 3));
+            //Repetition("ab", 3) ➞ "ababab"
+            //Repetition("kiwi", 1) ➞ "kiwi"
+            //Repetition("cherry", 2) ➞ "cherrycherry"
+        }
+        public static string Repetition(string txt, int n)
+        {
+            if (n == 0) return "";
+            else return txt + Repetition(txt, --n);
+        }
+    }
+}
---
+++ RecursiveStringsLength/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveStringsLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "apple";
+            Console.WriteLine(RecursiveStringLength(s));
+
+        }
+        static int StringLength(string s)
+        {
+            int count = 0;
+            for(int i=0; i<s.Length; i++)
+            {
+                count++;
+            }
+            return count;
+        }
+        static int RecursiveStringLength(string s)
+        {
+
+            if (s.Equals("")) return 0;
+            else return RecursiveStringLength(s.Substring(1)) + 1;
+        }
+
+    }
+}
