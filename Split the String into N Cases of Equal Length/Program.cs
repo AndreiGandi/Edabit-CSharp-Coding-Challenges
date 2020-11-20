a168b6fbd2d79914cb2159e39f2ed1c08d52b697
Andrei Gandila <gandilaandrei@gmail.com>
2020-11-21 00:50:23 +0200

    Push 15

---
+++ Split the String into N Cases of Equal Length/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_the_String_into_N_Cases_of_Equal_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
