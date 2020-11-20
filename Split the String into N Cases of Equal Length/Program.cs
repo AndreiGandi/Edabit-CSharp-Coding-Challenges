ede2b99ca4dd34510ac3368b93dca77d9f8fd497
Andrei Gandila <gandilaandrei@gmail.com>
2020-11-21 01:48:48 +0200

    Update Program.cs

--- Split the String into N Cases of Equal Length/Program.cs
+++ Split the String into N Cases of Equal Length/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Split_the_String_into_N_Cases_of_Equal_Length
 {
-    class Program
+    //Create a function that accepts string input and int cases as parameters where the string is split into N distinct cases of equal length as shown:
+    //SplitNCases("Strengthened", 6) ➞ { "St", "re", "ng", "th", "en", "ed" }
+    //SplitNCases("Unscrupulous", 2) ➞ { "Unscru", "pulous" }
+    //SplitNCases("Flavorless", 1) ➞ { "Flavorless" }
+class Program
     {
         static void Main(string[] args)
         {
+            string[] arr = SplitNCases("Strengthened", 6);
+        }
+        public static string[] SplitNCases(string input, int cases)
+        {
+            List<string> result = new List<string>();
+            if (input.Length % cases != 0) result.Add("Error");
+            else
+            {
+                int n = input.Length / cases;
+                while(input.Length != 0)
+                {
+                    string s = input.Substring(0, n);
+                    result.Add(s);
+                    input = input.Remove(0, n);
+                }
+            }
+
+
+
+            return result.ToArray();
         }
     }
 }
